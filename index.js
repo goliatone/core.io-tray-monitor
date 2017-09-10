7468d1a521ffb56541e3d943747f25da847fee6d
goliatone <burgosemi@gmail.com>
2017-09-10 19:31:55 -0400

    Add index

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-/**
- * Export module core.io-tray-monitor.
- *
- * @type {Function}
- */
-module.exports = require('./lib');
+'use strict';
+
+const {ipcRenderer} = require('electron');
+
+document.addEventListener('DOMContentLoaded', () => {
+    let n = new Notification('You did it!', {
+        body: 'Nice work.'
+    });
+
+    // Tell the notification to show the menubar popup window on click
+    n.onclick = () => { ipcRenderer.send('show-window') };
+});
