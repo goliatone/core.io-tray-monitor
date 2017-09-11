82de2f6c310776b0ecce19f799d16a14a0e94caf
goliatone <burgosemi@gmail.com>
2017-09-10 20:49:45 -0400

    Update index

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 const {ipcRenderer} = require('electron');
 
 document.addEventListener('DOMContentLoaded', () => {
+
     let n = new Notification('You did it!', {
         body: 'Nice work.'
     });
 
-    // Tell the notification to show the menubar popup window on click
-    n.onclick = () => { ipcRenderer.send('show-window') };
+    /*
+     * Tell the notification to show
+     * the menubar popup window on click
+     */
+    n.onclick = () => {
+        ipcRenderer.send('show-window');
+    };
 });
