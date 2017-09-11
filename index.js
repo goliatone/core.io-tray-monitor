0c996442ba4be6b2f77c9c92fbfe57d674f5d127
goliatone <burgosemi@gmail.com>
2017-09-11 17:09:00 -0400

    Manage mqtt URL from options

--- index.js
+++ index.js
@@ -4,22 +4,49 @@ const {ipcRenderer} = require('electron');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    let n = new Notification('You did it!', {
-        body: 'Nice work.'
-    });
+    let btn = document.getElementById('send');
+    let input = document.getElementById('mqtt');
 
-    /*
-     * Tell the notification to show
-     * the menubar popup window on click
-     */
-    n.onclick = () => {
-        ipcRenderer.send('show-window');
-    };
+    btn.addEventListener('click', (e) => {
+        updateConfig({mqtt: input.value});
+    });
 
     ipcRenderer.on('update', (type, event)=>{
         console.log('update', type, event);
         log(event.record.appId);
     });
+
+    let config = getConfig();
+
+    if(config){
+        sendOptionsToMain(config);
+    }
+
+    if(config.mqtt){
+        input.value = config.mqtt;
+    }
+
+    function updateConfig(config){
+        log('Config: %s' + JSON.stringify(config));
+        saveConfig(config);
+        sendOptionsToMain(config);
+    }
+
+    function sendOptionsToMain(config){
+        ipcRenderer.send('ui:options', config);
+    }
+
+    // let n = new Notification('You did it!', {
+    //     body: 'Nice work.'
+    // });
+
+    /*
+     * Tell the notification to show
+     * the menubar popup window on click
+     */
+    // n.onclick = () => {
+    //     ipcRenderer.send('show-window');
+    // };
 
     function log(txt){
         let content = document.getElementById('content');
@@ -28,3 +55,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     log('Initialize!');
 });
+
+function getConfig() {
+    let config = localStorage.getItem('_config');
+    if(!config) return false;
+    return JSON.parse(config);
+}
+
+function saveConfig(c={}) {
+    localStorage.setItem('_config', JSON.stringify(c));
+}
